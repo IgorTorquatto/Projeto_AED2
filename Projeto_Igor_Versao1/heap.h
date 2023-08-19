ae3e6a52850abc9fce6c900cb9c1f5e57f0165dc
Igor <igor.torquato.20@gmail.com>
2023-08-19 15:10:49 -0300

    organizando

---
+++ Projeto_Igor_Versao1/heap.h
@@ -0,0 +1,18 @@
+#ifndef HEAP_H
+#define HEAP_H
+#include "naveEspacial.h"
+
+// Definição da estrutura da heap
+typedef struct {
+    NaveEspacial *array;
+    int capacidade;
+    int tamanho;
+} Heap;
+
+Heap* criarHeap(int capacidade);
+void inserir(Heap *heap, NaveEspacial nave);
+void trocar(NaveEspacial *a, NaveEspacial *b);
+void ajustarParaCima(Heap *heap, int indice);
+
+#endif
+
