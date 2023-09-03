d255bd2f7b17ea4362a0b1822bb9f9486a97835a
Igor Torquato <igor.torquato.20@gmail.com>
2023-09-03 17:52:35 -0300

    funcionando, corrigir linhas

--- Principal - Copia/main.c
+++ Principal - Copia/main.c
@@ -1,50 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <locale.h>
 #include "naveEspacial.h"
 #include "heap.h"
+#include <string.h>
+
+// Função para ler e inserir uma nave do arquivo
+int inserirNaveDoArquivo(FILE *arquivo, Heap *heap) {
+    int id, prioridade, idade, identificadorUnico;
+    char nome[50], planetaOrigem[50];
+
+    if (fscanf(arquivo, "%d,%d,%49[^,],%d,%49[^,],%d,", &id, &prioridade, nome, &idade, planetaOrigem, &identificadorUnico) == 6) {
+        Passageiro passageiros[1];
+        passageiros[0].idade = idade;
+        strcpy(passageiros[0].nome, nome);
+        strcpy(passageiros[0].planetaOrigem, planetaOrigem);
+        passageiros[0].identificadorUnico = identificadorUnico;
+
+        Recurso recursos[10]; // Ajuste o tamanho do array conforme necessário
+        int numRecursos = 0;
+
+        while (fscanf(arquivo, "%49[^,],%d,", recursos[numRecursos].nomeRecurso, &recursos[numRecursos].quantidade) == 2) {
+            numRecursos++;
+        }
+
+        NaveEspacial nave = {id, prioridade, passageiros, 1, recursos, numRecursos, verificarDadosNave};
+        inserir(heap, nave);
+        return 1; // Indica que uma nave foi inserida
+    }
+    return 0; // Indica que não foi possível ler mais naves
+}
 
 int main() {
-	setlocale(LC_ALL,"Portuguese"); // Para corrigir os problemas de acentuação
-	srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios
+    setlocale(LC_ALL, "Portuguese"); // Para corrigir os problemas de acentuação
+    srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios
     Heap *heap = criarHeap(10);
-    int *a;
 
-    // Exemplo de criação de uma nave espacial
-    
-    Passageiro passageiros[2] = { //Quantidade de passageiros a ser criados
-        {"Passageiro1", 30, "Terra", 1}, //Preenchendo todos os atributos do scruct
-        {"Passageiro2", 25, "Marte", 2}
-    };
+    // Abra o arquivo de texto para leitura
+    FILE *arquivo = fopen("naves.txt", "r");
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return 1;
+    }
 
+    int navesLidas = 0;
 
-    Recurso recursos[2] = {
-        {"Comida", 100},
-        {"Água", 50}
-    };
+    // Leitura e inserção das naves a partir do arquivo
+    while (inserirNaveDoArquivo(arquivo, heap)) {
+        navesLidas++;
+    }
 
-	//Criação das naves
-    NaveEspacial nave = {1,15, passageiros, 2, recursos, 2, verificarDadosNave}; // id,prioridade,passageiros,num_passageiro,recursos,num_recursos,função de verificação
-   
-	//Inserção das naves no heap
-    inserir(heap, nave);
+    fclose(arquivo);
 
-    //Impressão
-    imprimir(heap);
+    // Impressão
+    if (navesLidas > 0) {
+        imprimir(heap);
 
-    //Construção da heap
-    constroiHeap(heap);
+        // Construção da heap
+        constroiHeap(heap);
 
-	//Remoção
-	while (heap->tamanho > 0) {
-    NaveEspacial nave = heap->array[0];
-    printf("Nave id %d saindo da fila com prioridade %d\n", nave.id, nave.prioridade);
-    remover(heap);
-	}
+        // Remoção
+        while (heap->tamanho > 0) {
+            NaveEspacial nave = heap->array[0];
+            printf("Nave id %d saindo da fila com prioridade %d\n", nave.id, nave.prioridade);
+            remover(heap);
+        }
+    } else {
+        printf("Nenhuma nave foi lida do arquivo.\n");
+    }
 
-
-	//Liberação
+    // Liberação
     free(heap->array);
     free(heap);
 
