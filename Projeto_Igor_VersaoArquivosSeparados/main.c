604d9db161d15ba3b10b08f88a3b80dbd994759e
Igor <igor.torquato.20@gmail.com>
2023-09-03 13:21:18 -0300

    adicionando arquivo de txt

--- Projeto_Igor_VersaoArquivosSeparados/main.c
+++ Projeto_Igor_VersaoArquivosSeparados/main.c
@@ -4,83 +4,103 @@
 #include <locale.h>
 #include "naveEspacial.h"
 #include "heap.h"
+#include <unistd.h>
 
-int main() {
-    setlocale(LC_ALL, "Portuguese");
-    srand(time(NULL));
-
-    Heap *heap = criarHeap(10);
-
-    // Leitura de dados de um arquivo
-    FILE *arquivo = fopen("dados.txt", "r");
+void carregarDadosDeArquivo(const char *nomeArquivo, Heap *heap) {
+    // Abrir o arquivo para leitura
+    FILE *arquivo = fopen(nomeArquivo, "r");
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo.\n");
-        return 1;
+        return;
     }
 
     char linha[100];
+    NaveEspacial nave;
+
     while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-        // Parse dos dados da linha e criação das naves espaciais
-        NaveEspacial nave;
+        // Verificar se é uma linha com ID e prioridade
+        if (sscanf(linha, "%d %d %s", &nave.id, &nave.prioridade, nave.nome) == 3) {
+            // Inicializar os campos de passageiros e recursos
+            nave.passageiros = NULL;
+            nave.numPassageiros = 0;
+            nave.recursos = NULL;
+            nave.numRecursos = 0;
 
-        // Use sscanf para analisar a linha do arquivo
-        sscanf(linha, "%d %d %s", &nave.id, &nave.prioridade, nave.nome);
+            // Adicionar a nave à heap
+            inserirDesordenado(heap, nave);
+        }
+        // Verificar se é uma linha de passageiro
+        else if (strstr(linha, "Passageiros:") != NULL) {
+            while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+                // Verificar se é o fim da seção de passageiros
+                if (strstr(linha, "Recursos:") != NULL) {
+                    break;
+                }
 
-        inserirDesordenado(heap, nave);
+                // Processar a linha de passageiro e adicionar aos passageiros da nave
+                Passageiro passageiro;
+                sscanf(linha, "%s %d %s %d", passageiro.nome, &passageiro.idade, passageiro.planetaOrigem, &passageiro.identificadorUnico);
+
+                // Expandir a lista de passageiros da nave
+                nave.numPassageiros++;
+                nave.passageiros = realloc(nave.passageiros, nave.numPassageiros * sizeof(Passageiro));
+                nave.passageiros[nave.numPassageiros - 1] = passageiro;
+            }
+        }
+        // Verificar se é uma linha de recurso
+        else if (strstr(linha, "Recursos:") != NULL) {
+            while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+                // Processar a linha de recurso e adicionar aos recursos da nave
+                Recurso recurso;
+                sscanf(linha, "%s %d", recurso.nomeRecurso, &recurso.quantidade);
+
+                // Expandir a lista de recursos da nave
+                nave.numRecursos++;
+                nave.recursos = realloc(nave.recursos, nave.numRecursos * sizeof(Recurso));
+                nave.recursos[nave.numRecursos - 1] = recurso;
+            }
+        }
     }
 
+    // Fechar o arquivo
     fclose(arquivo);
+}
 
-    int opcao;
-    do {
-        printf("\nSelecione uma opção:\n");
-        printf("1. Inserir nova nave espacial\n");
-        printf("2. Remover nave espacial de maior prioridade\n");
-        printf("3. Sair\n");
-        printf("Opção: ");
-        scanf("%d", &opcao);
+void imprimirNave(NaveEspacial *nave) {
+    printf("Nave %d, entrando na fila com prioridade %d com %d passageiros e %d recursos.\n", nave->id, nave->prioridade, nave->numPassageiros, nave->numRecursos);
+}
 
-        // Solicitar dados da nova nave espacial ao usuário e inserir
-        NaveEspacial novaNave;
+int main() {
+    // Inicializar a heap
+    Heap *heap = criarHeap(10);
+    int i;
 
-        switch (opcao) {
-            case 1:
-                // Leitura dos dados da nova nave espacial e inserção
-                printf("ID: ");
-                scanf("%d", &novaNave.id);
-                printf("Prioridade: ");
-                scanf("%d", &novaNave.prioridade);
-                
-                // Usando fgets para ler o nome da nave
-                printf("Nome: ");
-                fgets(novaNave.nome, sizeof(novaNave.nome), stdin);
-                novaNave.nome[strcspn(novaNave.nome, "\n")] = '\0'; // Remover a quebra de linha
-                
-                inserir(heap, novaNave);
-                break;
-            case 2:
-                // Remover nave espacial de maior prioridade
-                if (heap->tamanho > 0) {
-                    printf("Nave espacial de maior prioridade removida.\n");
-                    remover(heap);
-                } else {
-                    printf("A fila de prioridades está vazia.\n");
-                }
-                fflush(stdout);
-                break;
-            case 3:
-                // Sair do programa
-                break;
-            default:
-                printf("Opção inválida.\n");
-        }
+    // Carregar dados do arquivo
+    carregarDadosDeArquivo("dados.txt", heap);
 
-    } while (opcao != 3);
+    // Imprimir as naves espaciais carregadas
+    //imprimir(heap);
+    
+    // Imprimir as naves espaciais carregadas com atraso de 2 segundos entre cada nave
+	for (i = 0; i < heap->tamanho; i++) {
+    imprimirNave(&heap->array[i]);
+	}
+
 
     // Liberar memória
+    for (i = 0; i < heap->tamanho; i++) {
+        free(heap->array[i].passageiros);
+        free(heap->array[i].recursos);
+    }
     free(heap->array);
     free(heap);
 
     return 0;
 }
+
+
+
+
+
+
 
