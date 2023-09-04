87e0acae082613a3528d9a3af569943fe3d9b0ed
Igor <igor.torquato.20@gmail.com>
2023-09-04 10:37:01 -0300

    bug de ler só uma linha corrigido

--- Projeto_Igor_Versao2/main.c
+++ Projeto_Igor_Versao2/main.c
@@ -5,37 +5,51 @@
 #include "heap.h"
 #include <string.h>
 
-// Função para ler e inserir uma nave do arquivo
-int inserirNaveDoArquivo(FILE *arquivo, Heap *heap) {
-    int id, prioridade, idade, identificadorUnico;
-    char nome[50], planetaOrigem[50];
+// Função para ler e inserir todas as naves do arquivo
+int inserirTodasNavesDoArquivo(FILE *arquivo, Heap *heap) {
+    int navesInseridas = 0;
+    char linha[256]; // Tamanho suficiente para armazenar uma linha
 
-    if (fscanf(arquivo, "%d,%d,%49[^,],%d,%49[^,],%d,", &id, &prioridade, nome, &idade, planetaOrigem, &identificadorUnico) == 6) {
-        Passageiro passageiros[1];
-        passageiros[0].idade = idade;
-        strcpy(passageiros[0].nome, nome);
-        strcpy(passageiros[0].planetaOrigem, planetaOrigem);
-        passageiros[0].identificadorUnico = identificadorUnico;
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        int id, prioridade, idade, identificadorUnico;
+        char nome[50], planetaOrigem[50];
 
-        Recurso recursos[50];
-        int numRecursos = 0;
+        if (sscanf(linha, "%d,%d,%49[^,],%d,%49[^,],%d,", &id, &prioridade, nome, &idade, planetaOrigem, &identificadorUnico) == 6) {
+            Passageiro passageiros[1];
+            passageiros[0].idade = idade;
+            strcpy(passageiros[0].nome, nome);
+            strcpy(passageiros[0].planetaOrigem, planetaOrigem);
+            passageiros[0].identificadorUnico = identificadorUnico;
 
-        char recursoNome[50];
-        int quantidade;
+            Recurso recursos[50];
+            int numRecursos = 0;
 
-        // Leitura dos recursos separados por vírgulas
-        while (fscanf(arquivo, "%49[^,],%d,", recursoNome, &quantidade) == 2) {
-            strcpy(recursos[numRecursos].nomeRecurso, recursoNome);
-            recursos[numRecursos].quantidade = quantidade;
-            numRecursos++;
+            char recursoNome[50];
+            int quantidade;
+
+            // Agora, dividimos a linha em recursos separados por vírgulas
+            char *token = strtok(linha, ",");
+            while (token != NULL) {
+                token = strtok(NULL, ",");
+                if (token != NULL && sscanf(token, "%49[^,],%d", recursoNome, &quantidade) == 2) {
+                    strcpy(recursos[numRecursos].nomeRecurso, recursoNome);
+                    recursos[numRecursos].quantidade = quantidade;
+                    numRecursos++;
+                }
+            }
+
+            NaveEspacial nave = {id, prioridade, passageiros, 1, recursos, numRecursos, verificarDadosNave};
+            inserir(heap, nave);
+            navesInseridas++;
+        } else {
+            return 0; // Encerra o loop se não for possível ler mais naves
         }
-
-        NaveEspacial nave = {id, prioridade, passageiros, 1, recursos, numRecursos, verificarDadosNave};
-        inserir(heap, nave);
-        return 1; // Indica que uma nave foi inserida
     }
-    return 0; // Indica que não foi possível ler mais naves
+
+    return navesInseridas;
 }
+
+
 
 int main() {
     setlocale(LC_ALL, "Portuguese"); // Para corrigir os problemas de acentuação
@@ -48,12 +62,14 @@ int main() {
         return 1;
     }
 
-    int navesLidas = 0;
+    int navesLidas=0;
 
     // Leitura e inserção das naves a partir do arquivo
-    while (inserirNaveDoArquivo(arquivo, heap)) {
+    while (inserirTodasNavesDoArquivo(arquivo, heap)) {
         navesLidas++;
     }
+
+  //  printf(" Naves lidas :%d",navesLidas); para verificar  o erro de ler só uma nave do txt
 
     fclose(arquivo);
 
